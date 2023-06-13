1b3b6bd141f94f8084aa571e1ca10c3b35fe3fbb
ivaylo91 <ipenev91@gmail.com>
2023-06-13 21:28:50 +0300

    convert centuries to years,days,hours and minutes

---
+++ src/com/java/variables/CenturiesInMinutes.java
@@ -0,0 +1,22 @@
+package com.java.variables;
+
+import java.util.Scanner;
+
+public class CenturiesInMinutes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int centuries = Integer.parseInt(input.nextLine());
+
+        double years = centuries * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        System.out.printf("%d centuries= %.0f years= %.0f days= %.0f hours= %.0f minutes",
+                centuries,
+                years,
+                days,
+                hours,
+                minutes);
+    }
+}
