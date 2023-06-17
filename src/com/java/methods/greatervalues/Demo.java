0d03b295bd8173e2d273e4e5527f3a845f55b9f0
ivaylo91 <ipenev91@gmail.com>
2023-06-17 13:24:34 +0300

    Find Greater value of String, char and int values

--- src/com/java/methods/greatervalues/Demo.java
+++ src/com/java/methods/greatervalues/Demo.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //String values
         String first = input.nextLine();
         String second = input.nextLine();
+        //Char values
+        char ch1 = input.nextLine().charAt(0);
+        char ch2 = input.nextLine().charAt(0);
+        // IntegerValues
+        int val1 = Integer.parseInt(input.nextLine());
+        int val2 = Integer.parseInt(input.nextLine());
+
 
         GreaterValue greaterVal = new GreaterValue();
-        System.out.println(greaterVal.getMax(first, second));
+        System.out.println(greaterVal.getMax(first, second)); // output string values
+        System.out.println(greaterVal.getMax(ch1, ch2));     // output char values
+        System.out.println(greaterVal.getMax(val1, val2));   // output int values
+
     }
 }
--- src/com/java/methods/greatervalues/GreaterValue.java
+++ src/com/java/methods/greatervalues/GreaterValue.java
@@ -1,6 +1,12 @@
 package com.java.methods.greatervalues;
 
 public class GreaterValue {
+    /**
+     *
+     * @param firstNum
+     * @param secondNum
+     * @return maxValue of Integer values
+     */
     public int getMax(int firstNum, int secondNum) {
         if (firstNum > secondNum) {
             return firstNum;
@@ -8,6 +14,12 @@ public class GreaterValue {
         return secondNum;
     }
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return max value of two characters
+     */
     public char getMax(char first, char second) {
         if (Character.compare(first, second) >= 0) {
             return first;
@@ -15,6 +27,12 @@ public class GreaterValue {
         return second;
     }
 
+    /**
+     *
+     * @param first
+     * @param second
+     * @return max value of two String values
+     */
     public String getMax(String first, String second) {
         if (first.compareTo(second) >= 0) {
             return first;
