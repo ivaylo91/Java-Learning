8173a5fdbb81263fe5f10d7b05731f8486d2dc2d
ivaylo91 <ipenev91@gmail.com>
2023-06-24 11:18:12 +0300

    Sort List of products using Collections

---
+++ src/com/java/lists/ListOfProducts.java
@@ -0,0 +1,23 @@
+package com.java.lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListOfProducts {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        List<String> products = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            products.add(input.nextLine());
+        }
+        Collections.sort(products);
+
+        for (int i = 0; i < products.size(); i++) {
+            System.out.printf("%d.%s%n", i + 1, products.get(i));
+        }
+    }
+}
